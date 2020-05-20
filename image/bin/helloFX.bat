8c0ce5730d06fc7d1a7451f12976efd91b8d3f2a
Wojciech Kwaśniewicz <wojtek.kwasniewicz@gmail.com>
2020-05-20 12:59:42 +0200

    Cleaned up root directory

--- image/bin/helloFX.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set DIR="%~dp0"
-set JAVA_EXEC="%DIR:"=%\java"
-pushd %DIR% & %JAVA_EXEC% -Djdk.gtk.version=2 -p "%~dp0/../app" -m LogicCircuitSimulator/LogicCircuitSimulator.Launcher  %* & popd
