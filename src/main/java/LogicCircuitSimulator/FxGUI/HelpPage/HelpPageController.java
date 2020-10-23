917d49bc13cb2cf01a08494341bb83e6a0b5d9d3
Wojciech Kwaśniewicz <wojtek.kwasniewicz@gmail.com>
2020-10-23 16:32:02 +0200

    updated HelpPage.fxml

--- src/main/java/LogicCircuitSimulator/FxGUI/HelpPage/HelpPageController.java
+++ src/main/java/LogicCircuitSimulator/FxGUI/HelpPage/HelpPageController.java
@@ -3,11 +3,14 @@ package LogicCircuitSimulator.FxGUI.HelpPage;
 import LogicCircuitSimulator.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 
 public class HelpPageController {
+    public ScrollPane scrollPane;
+
     @FXML
     void initialize(){
-
+        scrollPane.setVvalue(1);
     }
 
 
